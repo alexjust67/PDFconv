748e18b370fe5593c035f730710864fab30c7540
alexjust67 <alexjusti67n@gmail.com>
2023-08-19 12:01:51 +0200

    Updated with fuctionality to detect image-text and correct it

--- pdfRead.py
+++ pdfRead.py
@@ -1,15 +1,15 @@
-#D:/Vstudio/Vscode/converter
+#D:/Vstudio/Vscode/PDFconv
 
 # importing required modules
 import PyPDF2
 import re
 
 # creating a pdf file object
-text_file = open("D:/Vstudio/Vscode/converter/data.txt", "w")
+text_file = open("D:/Vstudio/Vscode/PDFconv/data.txt", "w")
 
 parts = []
 # creating a pdf file object
-pdfFileObj = open('D:/Vstudio/Vscode/converter/praticaATP-IRaereo.pdf', 'rb')
+pdfFileObj = open('D:/Vstudio/Vscode/PDFconv/praticaATP-IRaereo.pdf', 'rb')
   
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfReader(pdfFileObj, strict=True)
@@ -27,6 +27,32 @@ def visitor_body(text, cm, tm, font_dict, font_size):
 for pageObj in pdfReader.pages:
     pageObj.extract_text(visitor_text=visitor_body)
     parts.append("\n")
+
+def deletewhite(parts,index):
+    parts.pop(index)
+    if parts[index]=='':
+        deletewhite(parts,index)
+
+count = [0,[]]
+
+def deletewhitefor(parts):
+    for i in range(len(parts)-1):
+        if parts[i]=='':
+            count[0]+=1
+        if count[0]==1:
+            count[1].append(i)
+        if count[0]==4:
+            count[0]=0
+            deletewhite(parts,count[1][-1])
+            deletewhitefor(parts)
+            break
+
+deletewhitefor(parts)
+
+#ask for user input
+for i in count[1]:
+    print("insert the text missing between these two phrases: \n",parts[i-1],"\n",parts[i+1])
+    parts[i]=input("insert the text: ")
 
 
 text_body = "".join(parts)
