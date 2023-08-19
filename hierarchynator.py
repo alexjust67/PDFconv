8a94adee54deb7af10dad6f4c30b4368bd019516
alexjust67 <alexjusti67n@gmail.com>
2023-08-19 12:34:48 +0200

    Added start of hierarchynator.py

---
+++ hierarchynator.py
@@ -0,0 +1,26 @@
+file_path="D:/Vstudio/Vscode/PDFconv/praticaATP-IRaereo.txt"
+
+file=open(file_path,'r')
+
+hier=[]
+
+phrases=[]
+
+for line in file:
+    if line=='\n':
+        continue
+    else:
+        phrases.append(line)
+
+def parse_file(phrases,hier):
+    for line in phrases:
+        if line.startswith('('):
+            hier.append([line])
+            parse_file(phrases[phrases.index(line)+1:],hier[-1])
+        else:
+            hier.append(line)
+    return hier
+
+parse_file(phrases,hier)
+
+print(hier)
